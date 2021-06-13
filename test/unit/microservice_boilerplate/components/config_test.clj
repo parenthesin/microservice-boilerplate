e99e0c5f239180131d081ff327ca00e3fbcaa01c
Rafael Delboni <rafadelboni@gmail.com>
2021-06-13 15:36:01 -0300

    test: Adds test to http and config components

---
+++ test/unit/microservice_boilerplate/components/config_test.clj
@@ -0,0 +1,20 @@
+(ns unit.microservice-boilerplate.components.config-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [com.stuartsierra.component :as component]
+            [matcher-combinators.test :refer [match?]]
+            [microservice-boilerplate.components.config :as components.config]))
+
+(defn- create-and-start-system!
+  [{:keys [config]}]
+  (component/start-system
+   (component/system-map :config config)))
+
+(deftest config-mock-component-test
+  (testing "config should return mocked config"
+    (let [system (create-and-start-system!
+                  {:config (components.config/new-config {:webserver/port 1234
+                                                          :env :test})})]
+
+      (is (match? {:config {:webserver/port 1234
+                            :env :test}}
+                  (:config system))))))
---
+++ test/unit/microservice_boilerplate/components/http_test.clj
@@ -0,0 +1,26 @@
+(ns unit.microservice-boilerplate.components.http-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [com.stuartsierra.component :as component]
+            [matcher-combinators.test :refer [match?]]
+            [microservice-boilerplate.components.http :as components.http]))
+
+(defn- create-and-start-system!
+  [{:keys [http]}]
+  (component/start-system
+   (component/system-map :http http)))
+
+(deftest http-mock-component-test
+  (testing "HttpMock should return mocked reponses and log requests in the atom"
+    (let [system (create-and-start-system!
+                  {:http (components.http/new-http-mock
+                          {"https://duckduckgo.com" {:status 200}})})]
+
+      (is (match? {:status 200}
+                  (components.http/request (:http system) {:url "https://duckduckgo.com"})))
+
+      (is (match? {:status 500}
+                  (components.http/request (:http system) {:url "https://google.com"})))
+
+      (is (match? ["https://duckduckgo.com"
+                   "https://google.com"]
+                  (map :url (deref (get-in system [:http :requests]))))))))
