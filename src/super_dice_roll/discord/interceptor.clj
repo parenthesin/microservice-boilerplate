00f370040d7587361777893b36a245f8a2c2eeb5
Rafael Delboni <rafadelboni@gmail.com>
2021-07-06 19:37:11 -0300

    fix: Interceptor intercepts now

--- src/super_dice_roll/discord/interceptor.clj
+++ src/super_dice_roll/discord/interceptor.clj
@@ -3,7 +3,6 @@
             [super-dice-roll.discord.security :as discord.security]))
 
 (defn valid-request-interaction? [public-key timestamp body signature]
-  (logs/log :info {:header {:sig signature :time timestamp} :public-key public-key :body body})
   (discord.security/verify-request public-key timestamp body signature))
 
 (defn authentication-interceptor []
@@ -13,12 +12,21 @@
                   {{:keys [config]} :components} request
                   app-public-key (get-in config [:config :discord :app-public-key])
                   x-signature-ed25519 (get-in ctx [:request :headers "x-signature-ed25519"])
-                  x-signature-timestamp (get-in ctx [:request :headers "x-signature-timestamp"])]
+                  x-signature-timestamp (get-in ctx [:request :headers "x-signature-timestamp"])
+                  raw-body (slurp (:raw-body ctx))
+                  is-valid-request? (valid-request-interaction? app-public-key
+                                                                x-signature-timestamp
+                                                                raw-body
+                                                                x-signature-ed25519)]
 
-              (if (valid-request-interaction? app-public-key
-                                              x-signature-timestamp
-                                              (slurp (:raw-body ctx))
-                                              x-signature-ed25519)
+              (logs/log :info {:header {:sig x-signature-ed25519
+                                        :time x-signature-timestamp}
+                               :public-key app-public-key
+                               :body raw-body
+                               :valid is-valid-request?})
+
+              (if is-valid-request?
                 ctx
-                (assoc ctx :response {:status 401
+                (assoc ctx :response {:headers {"Content-Type" "application/text"}
+                                      :status 401
                                       :body "invalid request signature"}))))})
