41bbebb4a89e9938cc55cb0b0132dc53ab8c903b
Rafael Delboni <rafadelboni@gmail.com>
2021-07-13 00:14:28 -0300

    test: Adds logic command parse tests

--- src/super_dice_roll/logic.clj
+++ src/super_dice_roll/logic.clj
@@ -3,25 +3,25 @@
             [schema.core :as s]
             [super-dice-roll.schemas.models :as schemas.models]))
 
-(defn int-or-arg [input arg]
-  (try 
+(defn- int-or-arg [input arg]
+  (try
     (Integer/parseInt input)
     (catch Exception _
       arg)))
 
-(defn parse-first-part
+(defn- parse-first-part
   [command]
   (-> command
       string/upper-case
       (string/split #"D")))
 
-(defn parse-modifiers [value modifier]
+(defn- parse-modifiers [value modifier]
   (let [parsed (string/split value (re-pattern (str "\\" modifier)))
         sign (if (= modifier \+) 1 -1)]
     {:dice (-> parsed first (int-or-arg 0))
      :modifier (-> parsed last not-empty (or 0) Integer/parseInt (* sign))}))
 
-(defn parse-second-part
+(defn- parse-second-part
   [command]
   (cond
     (string/index-of command \+) (parse-modifiers command \+)
@@ -30,9 +30,7 @@
 
 (s/defn roll-command->roll :- schemas.models/Roll
   [{:keys [command] :as roll-command} :- schemas.models/RollCommand]
-  (let [parse (parse-first-part command)
-        times (-> parse first (int-or-arg 1))
-        second-part (parse-second-part (last parse))]
+  (let [first-part (parse-first-part command)
+        second-part (parse-second-part (last first-part))
+        times (-> first-part first (int-or-arg 1))]
     (merge {:command roll-command :times times} second-part)))
-
-(roll-command->roll {:user {:id "aaa" :username "bbb" :nick "ccc"} :command "6d12+4"})
---
+++ test/unit/super_dice_roll/logic_test.clj
@@ -0,0 +1,25 @@
+(ns unit.super-dice-roll.logic-test
+  (:require [clojure.test :refer [deftest are testing use-fixtures]]
+            [matcher-combinators.test :refer [match?]]
+            [schema-generators.generators :as g]
+            [schema.test :as schema.test]
+            [super-dice-roll.logic :as logic]
+            [super-dice-roll.schemas.models :as schemas.models]))
+
+(use-fixtures :once schema.test/validate-schemas)
+
+(defn ->cmd [command]
+  (assoc
+   (g/generate schemas.models/RollCommand)
+    :command command))
+
+(deftest roll-command->roll-test
+  (testing "should parse commands into roll map to be rolled afterwards"
+    (are [command expected] (match? expected (logic/roll-command->roll (->cmd command)))
+      "d6"     {:times 01 :dice 06 :modifier  0}
+      "2d12"   {:times 02 :dice 12 :modifier  0}
+      "3d20+5" {:times 03 :dice 20 :modifier  5}
+      "15d4-5" {:times 15 :dice 04 :modifier -5}
+      "wololo" {:times 01 :dice 00 :modifier  0}
+      ""       {:times 01 :dice 00 :modifier  0})))
+
